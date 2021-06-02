894bdc122b7de3c6dba3f3ef08b2476565757986
dugiwarc <dugiwarc>
2021-06-02 21:28:47 +0200

    show only posts from users we follow;

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -62,11 +62,22 @@ class MicroPostController extends AbstractController
     /**
      * @Route("/", name="micro_post_index")
      */
-    public function index() : Response
+    public function index(TokenStorageInterface $tokenStorage) : Response
     {
+        $currentUser = $tokenStorage->getToken()->getUser();
+
+        if($currentUser instanceof User) {
+            $posts = $this->microPostRepository->findAllByUsers($currentUser->getFollowing());
+
+        } else {
+            $posts = $this->microPostRepository->findBy([],['time' => 'DESC']);
+
+        }
+
         $html = $this->render('micro-post/index.html.twig', [
-            'posts' => $this->microPostRepository->findBy([],['time' => 'DESC'])
+            'posts' => $posts
         ]);
+
 
         return new Response($html);
     }
