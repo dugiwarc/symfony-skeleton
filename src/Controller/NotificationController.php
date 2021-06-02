cbd07a15b7a296ce2ec6bf4b9e2242f6bdb80eba
dugiwarc <dugiwarc>
2021-06-02 23:57:49 +0200

    add notification badge;

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -36,7 +36,7 @@ class NotificationController extends AbstractController
      */
     public function unreadCount()
     {
-        new JsonResponse([
+        return new JsonResponse([
             'count' => $this->notificationRepository->findUnseenByUser($this->getUser())
         ]);
     }
