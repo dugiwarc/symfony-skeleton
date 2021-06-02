25bfdf2812734b22028bceddd848776e5fa313c1
dugiwarc <dugiwarc>
2021-06-02 22:30:56 +0200

    make sure user does not follow the same person multiply times;

--- src/Controller/FollowingController.php
+++ src/Controller/FollowingController.php
@@ -29,7 +29,7 @@ class FollowingController extends AbstractController
          */
         $currentUser = $this->getUser();
         if($userToFollow->getId() !== $currentUser->getId()) {
-            $currentUser->getFollowing()->add($userToFollow);
+            $currentUser->follow($userToFollow);
 
             $this->getDoctrine()->getManager()->flush();
         }
